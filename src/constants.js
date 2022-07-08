8ad1dea1c03a4d49d7af26f105ce2179fed678c9
Scatolin, Lucas <lucas@madeincode.com.br>
2022-07-08 16:18:08 -0300

    Firts commit

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+const constants = {
+        error:{
+        FILE_LENGTH_ERROR_MESSAGE: "The content length is invalid",
+        FILE_FIELDS_ERROR_MESSAGE: "The content headers is invalid"
+    }
+
+}
+
+module.exports = constants
